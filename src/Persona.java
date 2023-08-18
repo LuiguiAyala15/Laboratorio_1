96d26b10c252f84f38f75b3d3d1c4eb9e3d5e8d0
Luigui_Ayala15 <142622405+LuiguiAyala15@users.noreply.github.com>
2023-08-18 14:04:48 -0500

    Add files via upload

---
+++ src/Persona.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Persona {
+     
+    private String nombre;
+    private String apelido;
+    private String dni;
+    private int edad;
+
+    public Persona(String nombre, String apelido, String dni) {
+        this.nombre = nombre;
+        this.apelido = apelido;
+        this.dni = dni;
+    }
+
+    
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apelido
+     */
+    public String getApelido() {
+        return apelido;
+    }
+
+    /**
+     * @param apelido the apelido to set
+     */
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    /**
+     * @return the dni
+     */
+    public String getDni() {
+        return dni;
+    }
+
+    /**
+     * @param dni the dni to set
+     */
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+}
